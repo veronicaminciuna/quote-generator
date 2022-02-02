85ceb6e8bf390872463b8eafbf31309e08d653b3
Veronica Minciuna <veronicaminci@gmail.com>
2022-02-01 23:53:29 -0500

    getQuotes function

---
+++ script.js
@@ -0,0 +1,19 @@
+// Get quotes from API
+let apiQuotes = [];
+
+
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        console.log(apiQuotes);
+    } catch (error) {
+        
+    }
+}
+
+
+
+//run getQuotes function
+getQuotes();
