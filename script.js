58136dd34ef0bb838c51ece99d76d7b06508e3a9
Veronica Minciuna <veronicaminci@gmail.com>
2022-02-02 00:00:11 -0500

    fetch random quote from api

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 // Get quotes from API
 let apiQuotes = [];
 
+//show the new quote
+function newQuote() {
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
 
 async function getQuotes() {
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
-        console.log(apiQuotes);
+        newQuote();
     } catch (error) {
         
     }
