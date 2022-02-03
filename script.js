e09594d11e6aaa4219e8b705aec514c7ab35ddec
Veronica Minciuna <veronicaminci@gmail.com>
2022-02-02 23:52:57 -0500

    make quote dynamic

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
 // Get quotes from API
 let apiQuotes = [];
 
 //show the new quote
 function newQuote() {
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
-}
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
+    }
 
 async function getQuotes() {
     const apiUrl = 'https://type.fit/api/quotes';
